469acdceb409b8862b6bc7c696b6d3ae9a26d8c6
continentlu@sina.com <continentlu@sina.com>
2023-10-31 22:43:25 +0800

    再启程

--- unitboot-demo1/src/main/java/org/union/sbp/springdemo/config/UnitConfiguration.java
+++ unitboot-demo1/src/main/java/org/union/sbp/springdemo/config/UnitConfiguration.java
@@ -1,22 +1,24 @@
 package org.union.sbp.springdemo.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.union.sbp.springbase.adaptor.annoatation.UnitConfigComponent;
 
 
 @EnableConfigurationProperties(DataSourceProperties.class)
 @ComponentScan(value = {"org.union.sbp.springdemo.controller.**"}
-        // ,basePackageClasses = {JdbcTemplateAutoConfiguration.class, MailSenderAutoConfiguration.class, DataSourceAutoConfiguration.class}
+        ,basePackageClasses = {JdbcTemplateAutoConfiguration.class, MailSenderAutoConfiguration.class, DataSourceAutoConfiguration.class}
         )
-//@MapperScan(basePackages = {"org.union.sbp.springdemo.dao"})
-//@Import({MybatisAutoConfiguration.class,Knife4jConfiguration.class})
-//@UnitConfigComponent
+@MapperScan(basePackages = {"org.union.sbp.springdemo.dao"})
+@Import({MybatisAutoConfiguration.class,Knife4jConfiguration.class})
+@UnitConfigComponent
 public class UnitConfiguration  {
     public UnitConfiguration(){
         System.out.println("初始化spring单元配置类v3");
